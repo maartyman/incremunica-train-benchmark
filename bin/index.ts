2f8a1037255096dfd677049dbc9c741a2fd969d8
maartenvandenbrande <maarten.vdbrande@gmail.com>
2023-09-26 10:18:28 +0200

    added common config

--- bin/index.ts
+++ bin/index.ts
@@ -4,20 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 import {BenchmarkConfig} from "../lib/Types";
 const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
-function getJoinConfigPath(joinName: string) {
+function getJoinConfigPath(basePathName: string, joinName: string) {
   switch (joinName) {
     case "computational-bind-join":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/computational-bind-join/config.json";
+      return basePathName + "computational-bind-join/config.json";
     case "delta-query":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/delta-query/config.json";
+      return basePathName + "delta-query/config.json";
     case "full-hash-join":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/full-hash-join/config.json";
+      return basePathName + "full-hash-join/config.json";
     case "memory-bind-join":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/memory-bind-join/config.json";
+      return basePathName + "memory-bind-join/config.json";
     case "nestedloop-join":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/nestedloop-join/config.json";
+      return basePathName + "nestedloop-join/config.json";
     case "partial-hash-join":
-      return "/home/maarten/Documents/doctoraat/code/incremunica-trainbench/data/configs/partial-hash-join/config.json";
+      return basePathName + "partial-hash-join/config.json";
   }
   throw new Error(`join algorithm:${joinName} doesn't exist`);
 }
@@ -34,7 +34,7 @@ async function run(): Promise<void> {
     }
     if (benchmarkConfigsFilePath === "") throw new Error('benchmarkConfigsFilePath not specified, add -f tag!');
 
-    let benchmarkConfigs = await new Promise<any[]>((resolve, reject) => {
+    let benchmarkFile = await new Promise<any>((resolve, reject) => {
       fs.readFile(
         benchmarkConfigsFilePath,
         'utf8',
@@ -43,24 +43,24 @@ async function run(): Promise<void> {
             reject(error);
           }
           try {
-            resolve(JSON.parse(data).benchmarkConfigs);
+            resolve(JSON.parse(data));
           } catch (error) {
             reject(error);
           }
         });
     });
 
-    for (const configFile of benchmarkConfigs) {
+    for (const configFile of benchmarkFile.benchmarkConfigs) {
       let configFileName = configFile.joinAlgorithm + "_" + new Date().toUTCString() + "-" + new Date().getSeconds() + "," + new Date().getMilliseconds() + ".json";
 
-      let resultPath = configFile.baseResultPath + uuidv4() + ".csv";
+      let resultPath = benchmarkFile.commonConfig.baseResultPath + uuidv4() + ".csv";
 
       let benchmarkConfig: BenchmarkConfig = {
         matchTransformPercentage: configFile.matchTransformPercentage,
-        randomSeed: configFile.randomSeed,
-        queryEngineConfig: getJoinConfigPath(configFile.joinAlgorithm),
+        randomSeed: benchmarkFile.commonConfig.randomSeed,
+        queryEngineConfig: getJoinConfigPath(benchmarkFile.commonConfig.baseIncremunicaConfigPath, configFile.joinAlgorithm),
         dataPath: configFile.dataPath,
-        cachedResultsBasePath: configFile.cachedResultsBasePath,
+        cachedResultsBasePath: benchmarkFile.commonConfig.cachedResultsBasePath,
         operationStings: configFile.operationStings,
         resultsPath: resultPath,
         numberOfTransforms: configFile.numberOfTransforms,
@@ -68,14 +68,14 @@ async function run(): Promise<void> {
       };
 
       //save benchmark config
-      try {fs.mkdirSync(configFile.baseConfigPath) } catch (e) {}
-      fs.writeFileSync(configFile.baseConfigPath + configFileName, JSON.stringify(configFile));
+      try {fs.mkdirSync(benchmarkFile.commonConfig.baseConfigPath) } catch (e) {}
+      fs.writeFileSync(benchmarkFile.commonConfig.baseConfigPath + configFileName, JSON.stringify(configFile));
 
-      try { fs.rmSync(configFile.baseConfigPath + 'latest', {recursive: true}) } catch (e) {}
-      try { fs.mkdirSync(configFile.baseConfigPath + 'latest') } catch (e) {}
-      fs.writeFileSync(configFile.baseConfigPath + 'latest/' + configFileName,JSON.stringify(configFile, undefined, "\n"));
+      try { fs.rmSync(benchmarkFile.commonConfig.baseConfigPath + 'latest', {recursive: true}) } catch (e) {}
+      try { fs.mkdirSync(benchmarkFile.commonConfig.baseConfigPath + 'latest') } catch (e) {}
+      fs.writeFileSync(benchmarkFile.commonConfig.baseConfigPath + 'latest/' + configFileName,JSON.stringify(configFile, undefined, "\n"));
 
-      try {fs.mkdirSync(configFile.baseResultPath) } catch (e) {}
+      try {fs.mkdirSync(benchmarkFile.commonConfig.baseResultPath) } catch (e) {}
       fs.writeFileSync(resultPath, QueryRunner.BuildCSVHeader(configFile));
 
       //warmupRound
